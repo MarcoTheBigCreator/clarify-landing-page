1767d673a4d1acfbad80851561d7324cfa20fcb0
Marco Rodriguez <marcotbcreator@gmail.com>
2024-09-01 17:53:45 -0600

    chore: Update ProductPreview component styling and video source

--- src/components/ui/ProductPreview.tsx
+++ src/components/ui/ProductPreview.tsx
@@ -1,23 +1,26 @@
 export const ProductPreview = () => {
   return (
-    <div className="relative mx-auto bg-gray-900 rounded-[2.5rem] h-[600px] w-[300px] shadow-xl flex items-center justify-center">
-      <div className="absolute top-4 w-24 h-1.5 bg-gray-700 rounded"></div>{' '}
-      {/* Notch */}
-      <div className="absolute bottom-4 w-24 h-1.5 bg-gray-700 rounded"></div>{' '}
-      {/* Bottom speaker */}
-      <div className="relative w-[272px] h-[540px] bg-black rounded-[2rem] overflow-hidden">
-        <video
-          className="h-full w-full"
-          controls
-          controlsList="nodownload"
-          poster="https://res.cloudinary.com/dmlpgks2h/image/upload/v1725059400/clarify/cgmo8iboqpb4mckfldg3.jpg"
-        >
-          <source
-            src="https://res.cloudinary.com/dmlpgks2h/video/upload/v1725059424/clarify/fv8ivudtxxg8ufd0r9sb.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
+    <div className="relative mx-auto border-zinc-800 dark:border-zinc-800 bg-zinc-800 border-[14px] rounded-[2.5rem] h-[640px] w-[300px] shadow-xl drop-shadow-purple">
+      <div className="h-[32px] w-[3px] bg-zinc-800 dark:bg-zinc-800 absolute -left-[17px] top-[72px] rounded-l-lg"></div>
+      <div className="h-[46px] w-[3px] bg-zinc-800 dark:bg-zinc-800 absolute -left-[17px] top-[124px] rounded-l-lg"></div>
+      <div className="h-[46px] w-[3px] bg-zinc-800 dark:bg-zinc-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
+      <div className="h-[64px] w-[3px] bg-zinc-800 dark:bg-zinc-800 absolute -right-[17px] top-[142px] rounded-r-lg"></div>
+      <div className="rounded-[2rem] overflow-hidden w-[272px] h-[593px] bg-white dark:bg-zinc-800">
+        <div className="relative w-[272px] h-[605px] overflow-hidden">
+          <video
+            className="h-full w-full object-cover"
+            controls
+            controlsList="nodownload"
+            poster="https://res.cloudinary.com/dmlpgks2h/image/upload/v1725234522/clarify/xbwdrw6asxcfxks4zlqg.png"
+          >
+            <source
+              src="https://res.cloudinary.com/dmlpgks2h/video/upload/v1725233438/clarify/z1pnzxue9yeihhnbd6wn.mov"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="absolute bottom-1 left-20 w-28 h-1.5 bg-gray-500 rounded"></div>{' '}
       </div>
     </div>
   );
