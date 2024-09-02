81a90f39d1e0b71e3597a94dc26f94e8243a31df
Marco Rodriguez <marcotbcreator@gmail.com>
2024-09-01 19:35:34 -0600

    chore: Update SVG assets, Team component, ProductPreview component, Navbar styling, and Logo component

--- src/locales/team-members.ts
+++ src/locales/team-members.ts
@@ -2,8 +2,7 @@ export const teamMembers = [
   {
     name: 'Karely Durán',
     role: 'CEO, Full Stack',
-    image:
-      'https://res.cloudinary.com/dmlpgks2h/image/upload/v1725082716/clarify/cwj1dsarrrf06toaftzl.jpg',
+    image: '/team-members/karely.jpg',
     github: 'https://github.com/karelycode ',
     linkedin: 'https://www.linkedin.com/in/karelyd-code/',
     instagram: 'https://www.instagram.com/karely.dv/',
@@ -11,8 +10,7 @@ export const teamMembers = [
   {
     name: 'Eder Ceniceros',
     role: 'Experto en IA',
-    image:
-      'https://res.cloudinary.com/dmlpgks2h/image/upload/v1725226973/clarify/kiijmaxhoip15z5dxg9l.jpg',
+    image: '/team-members/eder.jpg',
     github: 'https://github.com/EderCeniceros',
     linkedin:
       'https://www.linkedin.com/in/eder-ricardo-ceniceros-solis-b53598276/',
@@ -21,8 +19,7 @@ export const teamMembers = [
   {
     name: 'Estefanía Leyva',
     role: 'Gerente de Negocios',
-    image:
-      'https://res.cloudinary.com/dmlpgks2h/image/upload/v1725082716/clarify/pvsptvwg0odsjlnjyxc4.jpg',
+    image: '/team-members/estefania.jpg',
     github: '',
     linkedin: '',
     instagram: 'https://www.instagram.com/estefanialeyva_1905/',
@@ -30,8 +27,7 @@ export const teamMembers = [
   {
     name: 'Jacqueline Moreno',
     role: 'Diseñadora UX/UI',
-    image:
-      'https://res.cloudinary.com/dmlpgks2h/image/upload/v1725223788/clarify/zdg0mz3edx2ci3rxerfq.jpg',
+    image: '/team-members/jacqueline.jpg',
     github: 'https://github.com/xSotoJacqueline',
     linkedin: 'https://mx.linkedin.com/in/jacqueline-moreno-soto-921a55325',
     instagram: 'https://www.instagram.com/xsotojacqueline/',
@@ -39,8 +35,7 @@ export const teamMembers = [
   {
     name: 'Brayan Pastrana',
     role: 'Mobile Developer',
-    image:
-      'https://res.cloudinary.com/dmlpgks2h/image/upload/v1725223783/clarify/bpzhrlyuklgniibuwjaq.jpg',
+    image: '/team-members/brayan.jpg',
     github: 'https://github.com/BrayanPastranaP',
     linkedin: 'https://www.linkedin.com/in/brayan-pastrana-002763326/',
     instagram: 'https://www.instagram.com/brayan.pastrana.perez/',
